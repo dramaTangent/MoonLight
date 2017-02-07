c7384e19fac585437c7452c1101ad4af8d84aece
root <root@localhost.localdomain>
2017-02-07 14:12:36 +0400

    half ended slider and modified animationjs

---
+++ scripts/animations.js
@@ -0,0 +1,18 @@
+function fade(what,func=null,from=1,to=0,tran=4000){
+    var opacity=from*10;
+    var inter=setInterval(function(){
+        if(from>to){
+            opacity--;
+        }else{
+            opacity++;
+        }
+        what.style.opacity=opacity/10;
+        if(opacity==to*10){
+            clearInterval(inter);
+            if(func!=null){
+                func();
+            }
+        }
+    },tran/100);
+}
+
--- scripts/slider.js
+++ scripts/slider.js
@@ -8,19 +8,69 @@ function childNodesToArray(childs){
     return rArray;
 }
 
-var imageIndex=0;
-var imageObjects
-
-var slider={
-    ImageIndex:{
-        currentValue:function(){return imageIndex},
-        increase:function(){imageIndex++},
-        decrease:function(){imageIndex--},
-        check:function(){   
-        //check it
-        } 
-    },
-    Images:{
-        objects:childNodesToArray(document.getElementById("MoonSliderImageList").childNodes)
+function Slider(className,index){
+    this.sliderIndex=index;
+    this.imageIndex=0;
+    this.sliderObject=document.getElementsByClassName(className)[this.sliderIndex];
+    this.sliderElements=childNodesToArray(this.sliderObject.childNodes);
+    this.sliderSwitchers=[];
+    this.sliderImages=null;
+    this.sliderImageList=[];
+    this.setSliderImages=function(){
+        for(var i=0;i<this.sliderElements.length;i++){
+            if(this.sliderElements[i].className.search("imageList")!=-1){
+                this.sliderImages=this.sliderElements[i];
+            }
+        }
     }
-}
+    this.setSliderImageList=function(){
+        this.setSliderImages();
+        for(var i=0;i<childNodesToArray(this.sliderImages.childNodes).length;i++){
+            this.sliderImageList[i]=childNodesToArray(this.sliderImages.childNodes)[i];
+        }
+    }
+    this.setSliderSwitchers = function(){
+        for(var i=0;i<this.sliderElements.length;i++){
+            if(this.sliderElements[i].className.search("switcher")!=-1){
+                if(this.sliderSwitchers[0]==undefined){
+                    this.sliderSwitchers[0]=this.sliderElements[i];
+                }else{
+                    this.sliderSwitchers[1]=this.sliderElements[i];
+                }
+            }
+        }
+    }
+    this.showImage=function(visible){
+        for(var i=0;i<this.sliderImageList.length;i++){
+            this.sliderImageList[i].style.display="none";
+        }
+        this.sliderImageList[visible].style.display="block";
+    }
+    this.setEventsOnSwitchers=function(){
+        this.setSliderSwitchers();
+        this.setSliderImages();
+        this.setSliderImageList();
+        this.slideFade=function(){
+            
+        }
+        this.sliderSwitchers[0].addEventListener("click",function(){
+            slider.imageIndex--;
+            fade(slider.sliderImages,function(){
+                slider.showImage(slider.imageIndex);
+                fade(slider.sliderImages,null,0,1);
+            });
+        });
+        this.sliderSwitchers[1].addEventListener("click",function(){
+            slider.imageIndex++;
+            fade(slider.sliderImages,function(){
+                slider.showImage(slider.imageIndex);
+                fade(slider.sliderImages,null,0,1);
+            });
+        });
+    }
+};
+
+
+var slider=new Slider("MoonSlider",0);
+slider.setEventsOnSwitchers();
+slider.showImage(0);
