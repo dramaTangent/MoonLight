de86c1cfa3884ef5286bf94357a4046cf8fa22a0
root <root@localhost.localdomain>
2017-02-07 11:46:31 +0400

    trying js slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,11 +1,26 @@
+function childNodesToArray(childs){
+    var rArray=[];
+    var q=0
+    for(var i=1;i<childs.length;i+=2){
+        rArray[q]=childs[i];
+        q++
+    }
+    return rArray;
+}
 
 var imageIndex=0;
-var ImageIndex={
-    currentValue:imageIndex,
-    increase:function(){imageIndex++},
-    decrease:function(){imageIndex--},
-    check:function(){   
+var imageObjects
+
+var slider={
+    ImageIndex:{
+        currentValue:function(){return imageIndex},
+        increase:function(){imageIndex++},
+        decrease:function(){imageIndex--},
+        check:function(){   
         //check it
+        } 
+    },
+    Images:{
+        objects:childNodesToArray(document.getElementById("MoonSliderImageList").childNodes)
     }
 }
-console.log(imageIndex);
