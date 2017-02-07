0aba61087ad94b5cecd076ee6d8f199b980e9d36
root <root@localhost.localdomain>
2017-02-07 11:10:48 +0400

    working on sliderjs

---
+++ scripts/slider.js
@@ -0,0 +1,11 @@
+
+var imageIndex=0;
+var ImageIndex={
+    currentValue:imageIndex,
+    increase:function(){imageIndex++},
+    decrease:function(){imageIndex--},
+    check:function(){   
+        //check it
+    }
+}
+console.log(imageIndex);
